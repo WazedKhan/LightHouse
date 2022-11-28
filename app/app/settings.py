fd1fbc25ee320022efb498c843023fc037eabd34
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-11-28 21:11:53 +0600

    create user api

--- app/app/settings.py
+++ app/app/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'drf_spectacular',
 
     'core',
+    'user',
 ]
 
 MIDDLEWARE = [
--- app/app/urls.py
+++ app/app/urls.py
@@ -18,7 +18,7 @@ from drf_spectacular.views import (  # type:ignore
     )
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,5 +27,7 @@ urlpatterns = [
         'api/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs'
-        )
+        ),
+
+    path('api/user/', include('user.urls')),
 ]
