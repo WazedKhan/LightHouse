8def95217baed87461d5fc57ef5b6a57712eda57
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-11-28 13:20:08 +0600

    tests for checking db connection

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,12 @@
+"""
+Command for wait until DB connection stabilized
+"""
+
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    """Commands: wait for DB"""
+
+    def handle(self, *args, **options):
+        pass
