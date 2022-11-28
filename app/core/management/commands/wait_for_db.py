986084b5fcd702a94e4a2f50796402e969350a91
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-11-28 13:37:26 +0600

    wait_for_db_connection command

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,7 +1,11 @@
 """
 Command for wait until DB connection stabilized
 """
+import time
 
+from psycopg2 import OperationalError as Psycopg2OpError  # type: ignore
+
+from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
 
@@ -9,4 +13,15 @@ class Command(BaseCommand):
     """Commands: wait for DB"""
 
     def handle(self, *args, **options):
-        pass
+        self.stdout.write('Waiting for DB connection..')
+        db_up = False
+
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg2OpError, OperationalError):
+                self.stdout.write(self.style.WARNING('Database Connection failed, reconnecting in 1 sec..'))  # noqa
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS('DB connection stabilized.'))
