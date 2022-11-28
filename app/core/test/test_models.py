7c49850538ca98ec640cddf31a8eed26499a93a2
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-11-28 14:06:53 +0600

    UserManager - create_superuser

--- app/core/test/test_models.py
+++ app/core/test/test_models.py
@@ -31,3 +31,18 @@ class ModelTests(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')  # type: ignore # noqa
             self.assertEqual(user.email, expected)
+
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without an email raises a ValueError."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')  # type: ignore
+
+    def test_create_superuser(self):
+        """Test creating a superuser."""
+        user = get_user_model().objects.create_superuser(  # type: ignore
+            'test@example.com',
+            'test123',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
